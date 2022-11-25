c960aef02e364cf3cf1440919527502e2d629d1a
winifredubom <winifredubom@gmail.com>
2022-11-25 23:35:56 +0100

    changes

--- lib/screens/onBoard_screen.dart
+++ lib/screens/onBoard_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
-class OnBoard extends StatefulWidget {
+class OnBoard extends StatelessWidget {
   const OnBoard({Key? key}) : super(key: key);
 
   @override
-  State<OnBoard> createState() => _OnBoardState();
-}
-
-class _OnBoardState extends State<OnBoard> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(child: IntroductionScreen(
+        pages: [
+          PageViewModel(
+            title:'Find Pharmacy near you',
+            body: 'it\'s easy to find pharmacy that is near to your location',
+            image: Image.asset('images/find_a_pharmacy_near_you.jpg'),
+          ),
+          PageViewModel(
+            title:'Search with our database',
+            body: 'Find the nearest pharmacy close to you',
+            image: Image.asset('images/pharmacy_finder.jpg') ,
+          ),
+          PageViewModel(
+            title:'Get delivery to your Doorstep',
+            body: 'Get your drugs delivered to your doorstep' ,
+            image: Image.asset('images/medicine_delivery.png'),
+          ),
+
+        ], //onDone: onDone, done: done
+     ));
+
+
   }
 }
+
